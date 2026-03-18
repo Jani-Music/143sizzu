e3ab027c31a043491709266adada75944f2396c4
Jani-Music <hahahsygs395@gmail.com>
2026-03-18 09:33:07 +0530

    Update thumbnails.py

--- IstkharMusic/utils/thumbnails.py
+++ IstkharMusic/utils/thumbnails.py
@@ -137,7 +137,7 @@ async def get_thumb(videoid):
             stroke_fill="black",
         )
 
-        stats_text = f"YouTube : {views} | Time : {duration} | Player : @SandhiyaMusicBot"
+        stats_text = f"YouTube : {views} | Time : {duration} | Player : @SizzuMusicBot"
         w_stats = get_text_width(stats_text, font_details)
         draw.text(
             ((1280 - w_stats) / 2, text_y_pos + 70),
@@ -162,7 +162,7 @@ async def get_thumb(videoid):
 
         draw.text(
             (30, 680),
-            text="@govindyadav1431",
+            text="@Power_of_Jani",
             fill="white",
             font=font_watermark,
             stroke_width=1,
