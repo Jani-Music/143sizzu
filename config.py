4755c3c39a623d24f68876cb8449c3f88a0922f8
Jani-Music <hahahsygs395@gmail.com>
2026-03-18 09:23:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ======================================================
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/JaniRPLover/Jani-RP-143sizzu")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Jani-Music/143sizzu")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
